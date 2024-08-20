d500196889b36b192bc2bc9721db6b8d643ec904
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-08-19 23:51:59 -0300

    Add files via upload

---
+++ projeto_principal/database/cliente.py
@@ -0,0 +1,7 @@
+INFO_CLIENTE = [
+    {"id": 1, "nome": "Guilhedatame",'data_Registro': '2024-08-03', 'telefone': '44 99761-4933'},
+    {"id": 2, "nome": "Jodatage", 'data_Registro': '2024-08-03', 'telefone': '44 99761-4933'},
+    {"id": 3, "nome": "Madataia", 'data_Registro': '2024-08-03', 'telefone': '44 99761-4933'},
+    {"id": 4, "nome": "Felipe",'data_Registro': '2024-08-03', 'telefone': '44 99761-4933'},
+    {"id": 5, "nome": "Joao", 'data_Registro': '2024-08-03', 'telefone': '44 99761-4933'},
+]
