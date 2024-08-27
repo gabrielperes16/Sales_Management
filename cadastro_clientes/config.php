ced875dc424a164fb6ce018f36f2e7ab1b705449
Gabriel da Costa Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-08-27 00:44:48 -0300

    Delete cadastro_clientes/config.php

--- cadastro_clientes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $hostname = 'localhost';
-    $usuario= 'root';
-    $senha= '';
-    $bancodedados = 'clientes';
-    $conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
-?>
