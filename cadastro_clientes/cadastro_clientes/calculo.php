8f0c6b5676cc001d5c7a01af57f24c117fd06ce0
gabriel Peres <105747318+gabrielperes16@users.noreply.github.com>
2024-09-02 16:16:06 -0300

    Add files via upload

--- cadastro_clientes/cadastro_clientes/calculo.php
+++ cadastro_clientes/cadastro_clientes/calculo.php
@@ -59,42 +59,69 @@ $result = mysqli_query($conexao, $sql);
 <body>
 
 <div class="m-5">
-        <table class="box table m-3 table-striped table-dark position-relative top-0 start-0 rounded-3">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Telefone</th>
-                    <th scope="col">Gênero</th>
-                    <th scope="col">Quantidade</th>
-                    <th scope="col">Data de Registro</th>
-                    <th scope="col">Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-<?php
-while ($user_data = mysqli_fetch_assoc($result)) {
-    $quantidade = $user_data['quantidade'];
-    $nome = $user_data['nome'];
-    echo "<tr>";
-    echo "<td>" . $user_data['id'] . "</td>";
-    echo "<td>" . $user_data['nome'] . "</td>";
-    echo "<td>" . $user_data['telefone'] . "</td>";
-    echo "<td>" . $user_data['sexo'] . "</td>";
-    echo "<td>" . $user_data['quantidade'] . "</td>";
-    echo "<td>" . $user_data['data_registro'] . "</td>";
-    echo "<td>
-        <a class='btn btn-success' href='#' data-bs-toggle='modal' data-bs-target='#calculoModal'data-nome='$nome' data-quantidade='$quantidade' title='Calcular'>
-        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-calculator-fill' viewBox='0 0 16 16'>
-            <path d='M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm2 .5v2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5m0 4v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5zM4.5 9a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 12.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5M7.5 6a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM7 9.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5m.5 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM10 6.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5m.5 2.5a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-.5-.5z'/>
-        </svg>
-        </a>
-    </td>";
-    echo "</tr>";
-}
-?>
+    <table class="box table m-3 table-striped table-dark position-relative top-0 start-0 rounded-3">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Telefone</th>
+                <th scope="col">Gênero</th>
+                <th scope="col">Quantidade</th>
+                <th scope="col">Data de Registro</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($user_data = mysqli_fetch_assoc($result)) {
+                $quantidade = $user_data['quantidade'];
+                $nome = $user_data['nome'];
+                echo "<tr>";
+                echo "<td>" . $user_data['id'] . "</td>";
+                echo "<td>" . $user_data['nome'] . "</td>";
+                echo "<td>" . $user_data['telefone'] . "</td>";
+                echo "<td>" . $user_data['sexo'] . "</td>";
+                echo "<td>" . $user_data['quantidade'] . "</td>";
+                echo "<td>" . $user_data['data_registro'] . "</td>";
+                echo "<td>
+                    <a class='btn btn-success' href='#' data-bs-toggle='modal' data-bs-target='#calculoModal' data-nome='$nome' data-quantidade='$quantidade' title='Calcular'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-calculator-fill' viewBox='0 0 16 16'>
+                            <path d='M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm2 .5v2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5m0 4v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5zM4.5 9a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 12.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5M7.5 6a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM7 9.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5m.5 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM10 6.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5m.5 2.5a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-.5-.5z'/>
+                        </svg>
+                    </a>
+                    <a class='button' href='#' data-bs-toggle='modal' data-bs-target='#SomaModal' title='Soma'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='37' height='45' fill='currentColor' class='bi bi-plus-square-fill' viewBox='0 0 16 16'>
+                            <path d='M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0'/>
+                        </svg>
+                    </a>
+                </td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
 
-<!-- Modal -->
+<!-- Modal de Soma -->
+<div class="modal fade" id="SomaModal" tabindex="-1" aria-labelledby="somaModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="somaModalLabel">Soma de todos os Valores</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p id="modal-pessoas"></p>
+                <p id="modal-total"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal de Cálculo -->
 <div class="modal fade" id="calculoModal" tabindex="-1" aria-labelledby="calculoModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -117,10 +144,30 @@ while ($user_data = mysqli_fetch_assoc($result)) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
-        var modalElement = document.getElementById('calculoModal');
+        var modalElement = document.getElementById('SomaModal');
         var modal = new bootstrap.Modal(modalElement);
         
-        modalElement.addEventListener('show.bs.modal', function (event) {
+        function atualizarModal() {
+            var buttons = document.querySelectorAll('[data-quantidade]');
+            var totalQuantidade = 0;
+
+            buttons.forEach(function(button) { // Adicionado o parêntese de fechamento aqui
+                var quantidade = parseFloat(button.getAttribute('data-quantidade')) || 0;
+                totalQuantidade += quantidade;
+            });
+
+            var resultado = totalQuantidade * 7.00;
+
+            document.getElementById('modal-pessoas').innerHTML = 'A Total de Itens este Mês é : <strong>' + totalQuantidade.toFixed(2) + '</strong>.';
+            document.getElementById('modal-total').innerHTML = 'A Soma de Todos os Valores é: <strong>R$ ' + resultado.toFixed(2).replace('.', ',') + '</strong>.';
+        }
+
+        modalElement.addEventListener('show.bs.modal', atualizarModal);
+
+        var calculoModalElement = document.getElementById('calculoModal');
+        var calculoModal = new bootstrap.Modal(calculoModalElement);
+        
+        calculoModalElement.addEventListener('show.bs.modal', function (event) {
             var button = event.relatedTarget;
             var nome = button.getAttribute('data-nome');
             var quantidade = button.getAttribute('data-quantidade');
@@ -128,6 +175,9 @@ while ($user_data = mysqli_fetch_assoc($result)) {
             document.getElementById('modal-nome').innerHTML = '<strong>' + nome + '</strong> realizou compras neste mês.';
             document.getElementById('modal-quantidade').innerHTML = 'O total de unidades adquiridas foi de <strong>' + quantidade + '</strong>.';
             document.getElementById('modal-resultado').innerHTML = 'O valor total gasto em litros de leite foi de <strong>R$ ' + resultado.toFixed(2).replace('.', ',') + '</strong>.';
-            });
+        });
     });
 </script>
+
+</body>
+</html>
--- cadastro_clientes/cadastro_clientes/sistema.php
+++ cadastro_clientes/cadastro_clientes/sistema.php
@@ -33,6 +33,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <style> 
+.tabela1{
+    background-color: rgba(0, 0, 0, 0.7);
+}
 .box {
     position: absolute;
     top: 30%;
@@ -115,7 +118,7 @@
         </button>
     </div>
     <div class='m-5'>
-        <table class="table m-3 table-striped table-dark position-relative ">
+        <table class="tabela1 table m-3 table-striped table-dark position-relative ">
             <thead>
                 <tr>
                     <th scope="col">#</th>
